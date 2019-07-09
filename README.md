b85322fe6a31eeca9aa515272f4d299cbb75b4b5
Tobi MacBook <bohn.m.tobias@gmail.com>
2019-07-09 10:07:50 +0200

    2019-07-08

--- README.md
+++ README.md
@@ -3,9 +3,15 @@
 Es ist ein reines Hobby-Projekt für die Schokobande von JP Performance
 
 ## Changelog
-    ** 2019-06-04 **
-    - Adding the project
+    ** 2019-07-04 **
+    - Projekt launch
 
-    ** 2019-06-05 **
-    - Add: json-Data
-    - Delete: database
+    ** 2019-07-05 **
+    - .json-Datei einbau
+    - Datenbank entfernt
+
+    ** 2019-07-08 **
+    - Camaro Fahrwerk Zeit hinzugefügt (wurde übersehen)
+    - KTM X-Bow Zeit hinzugefügt
+    - Änderungsdatum in der .json angepasst
+    - Footer Text Rechtschreibung
--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,7 @@
 
     <footer class="footer mt-auto py-3 bg-white">
         <div class="container text-center">
-            <p>Es ist ein Hobby-Projekt und wird nicht Verwaltet von JP. Es dient nur uns Schokohasen dazu ein Überblick zu haben.</p>
+            <p>Es ist ein Hobby-Projekt und wird nicht Verwaltet von JP. Es dient nur uns Schokohasen dazu einen Überblick zu haben.</p>
             <p><strong>Letzte Änderung:</strong> <?php echo date('d.m.Y', strtotime($json_data["lastupdate"])); ?></p>
             <a href="https://github.com/TobiasBohn/lasise">GitHub</a> - 
             <a href="/datenschutz.php">Datenschutz</a> - 
